89475922bb9e58bce6d7a2af6912b0bddfb06ddd
root <jasser.garcia>
2020-03-23 12:17:09 -0500

    Firts commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ubuntukioskmode
+# Ubuntu kiosk mode
+This script enable an advanced kiosk mode and open firefox with a URL pre configured , 
+this was created for atandt a requirement in an upwork contract.
+
+# Previous requirements
+Have Ubuntu desktop 18.04 
+
+# How to install
+
+- clone the respository and run run the script `sudo sh enablekioskmode.sh` (as root)
+- sudo reboot
+
+# Important notes
+If you are testing please perform this operations using a VM 
+
+# Future improvements
+start firefox in full screen
---
+++ enablekioskmode.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USR=$SUDO_USER
+
+apt-get update -y
+apt-get install -y --no-install-recommends openbox pulseaudio freerdp2-x11 gdm3
+usermod -a -G audio $USR
+mv /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.old
+#here config apps for autostart
+cat > /etc/xdg/openbox/autostart <<EOF
+#xfce-mcs-manager &
+/usr/bin/firefox https://www.google.com &
+EOF
+mv /etc/gdm3/custom.conf /etc/gdm3/custom-old.conf
+cat > /etc/gdm3/custom.conf <<EOF
+[daemon]
+#WaylandEnable=false
+# Enabling automatic login
+AutomaticLoginEnable = true
+AutomaticLogin = $USR
+# Enabling timed login
+#  TimedLoginEnable = true
+#  TimedLogin = user1
+#  TimedLoginDelay = 10
+[security]
+[xdmcp]
+[chooser]
+EOF
+
+cat > /var/lib/AccountsService/users/$USR <<EOF
+[InputSource0]
+xkb=es
+[User]
+XSession=openbox
+SystemAccount=false
+EOF
+cat > /etc/xdg/openbox/menu.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<openbox_menu xmlns="http://openbox.org/"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://openbox.org/
+                file:///usr/share/openbox/menu.xsd">
+<menu id="root-menu" label="Openbox 3">
+  <item label="Web browser">
+  e <action name="Execute"><execute>x-www-browser</execute></action>
+  </item>
+  <separator />
+  <item label="Exit">
+    <action name="Exit" />
+  </item>
+</menu>
+</openbox_menu>
+EOF
